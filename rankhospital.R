75e9fd063a5d0c67c14b8ce54d17b8ceedc20b19
Sam Friedman <sam.friedman@gmail.com>
2015-10-29 22:00:20 -0400

    First draft complete, ready to submit

--- rankhospital.R
+++ rankhospital.R
@@ -15,13 +15,39 @@ rankhospital <- function(p.state, p.outcome, p.num = "best") {
     stop("invalid state")
   } 
   
-  ## Read outcome data
+  ## Index and index outcome data
   outcome.index <- acceptable.p.outcome[p.outcome]
   
-  outcome.state <- outcome[outcome[,"State"]==p.state, ]
+  ## Coerce characters into numbers
+  outcome[,outcome.index] <- as.numeric(outcome[,outcome.index])
+  
+  hospital.index <- 2
+  state.index <- 7
+  
+  outcome <- subset(outcome
+                    , State == p.state & !is.na(outcome[,outcome.index])
+                    , select = c(hospital.index, outcome.index) )
+  
+  ## Reset indexes based on order in anon vector of select param above
+  hospital.index <- 1
+  outcome.index <- 2
+  
+  ## Check that p.num is valid
+  if( is.numeric(p.num) & p.num > nrow(outcome) ) {
+    return(NA)
+  }
+  else if(!is.numeric(p.num) ) {
+    if( !any( c("best","worst" ) == p.num) ) { return(NA) }
+    else if( p.num == "best" ) { p.num <- 1 }
+    else if( p.num == "worst" ) { p.num <- nrow(outcome) }
+  }
+  
+  # Rank outcome
+  outcome <- outcome[order(outcome[,outcome.index], outcome[,hospital.index]), ]
   
   ## Return hospital name in that state with the given rank
   ## 30-day death rate
+  return(outcome[p.num, hospital.index])
 }
 
 
