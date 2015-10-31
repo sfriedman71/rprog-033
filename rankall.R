80fb3abd0dd540a498b8df436d51a8e17dbabc4f
Sam Friedman <sam.friedman@gmail.com>
2015-10-30 22:30:51 -0400

    better comments

--- rankall.R
+++ rankall.R
@@ -1,80 +1,76 @@
-rankall <- function(p.outcome, p.num = "best") {
-  ## Read outcome data
-  ## Check that state and outcome are valid
-  ## For each state, find the hospital of the given rank
-  ## Return a data frame with the hospital names and the
-  ## (abbreviated) state name
+rankall <- function(outcome, num = "best") {
   
   ## Read outcome data
-  outcome.all <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings = "Not Available")
+  data <- read.csv("outcome-of-care-measures.csv"
+                   , colClasses = "character"
+                   , na.strings = c("Not Available"))
   
+  ## Map of outcome params and data columns
+  outcome.map <- c("heart attack" = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+                   , "heart failure" = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+                   , "pneumonia" = "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia" )  
   
-  ## Check that state and outcome are valid
-  acceptable.p.outcome = c("heart attack","heart failure","pneumonia")
-  if (!p.outcome %in% acceptable.p.outcome) { stop("invalid outcome")}
+  ## Check whether outcome if valid
+  if( ! outcome %in% names(outcome.map) ) { stop( "invalid outcome" ) }
   
-  state.list = sort(unique(outcome.all[,"State"]))
+  outcome.name <- outcome.map[[outcome]]
   
-  ## convert outcome name into column name
-  acceptable.v.outcome <- c("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
-  outcome.column <- acceptable.v.outcome[match(p.outcome, acceptable.v.outcome)]
+  ## Coerce characters into numbers only for the outome we are interested in
+  data[,outcome.name] <- as.numeric(data[,outcome.name])
   
   ## For each state, find the hospital of the given rank
-  hospital.state.list <- character(0)
+  ## The sort order is a requirement for the assignment
+  state.list <- sort( unique( data$State ) )
   
-  for (i in seq_along(state.list)) {
-    ## Return hospital name in that state with the given rank 30-day death rate
-    outcome.state <- outcome.all[outcome.all$State==state.list[i],]
+  ## capture return data here, later put it in a data frame
+  hospital.vector <- character(0)
+  
+  for( i in seq_along(state.list) ) {
     
-    # order data by outcome
-    #outcome.state <- outcome.state[order(as.numeric(outcome.state[[outcome.column]]),outcome.state[["Hospital.Name"]],decreasing=FALSE,na.last=NA), ]
-    outcome.state <- outcome.state[order(outcome.state[[outcome.column]], outcome.state[["Hospital.Name"]]), ]
+    state.name <- state.list[i]
     
-    #handle num input
-    this.num = p.num
-    if (this.num=="best") this.num = 1
-    if (this.num=='worst') this.num = nrow(outcome.state)
+    state.data <- subset( data
+                          ##, data$State == state.name & !is.na( data[outcome.name] )
+                          , data$State == state.name 
+                          , c("Hospital.Name", outcome.name)  )
     
-    hospital.state.list[i] <- outcome.state[this.num,"Hospital.Name"]
+    ## Sort state data by outcome values for rank
+    ## And by hospital name to break ties
+    state.data.sorted <- state.data[order( state.data[outcome.name]
+                                           , state.data[["Hospital.Name"]]
+                                           ,  decreasing = FALSE
+                                           , na.last = NA)
+                                    , ]
+    
+    ## check num param
+    ## I should later add logic handle bad values of num (i.e. num<0, num>nrow, bad chars)
+    state.num = num
+    if( num == "best") { state.num <- 1 }
+    if( num == "worst" ) { state.num <- nrow( state.data.sorted ) }
+    
+    ## Use the num (rank) to pick the row
+    ## Use the Hospital Names as the value
+    ## Add it to a list
+    hospital.vector[i] <- state.data.sorted[state.num,"Hospital.Name"] 
+    
   }
   
-  ## Return a data frame with the hospital names and the state name
-  data.frame(hospital=hospital.state.list,state=state.list,row.names=state.list)
+  ## Return a data frame with the hospital names and the
+  ## (abbreviated) state name
+  ## This is why we needed the two vectors to be in the same order
+  data.frame( hospital = hospital.vector
+              , state  = state.list
+              , row.names = state.list )
 }
-  
 
 qc <- function() {
-head(rankall("heart attack", 20), 10)
-
-  #hospital state
-  #AK <NA> AK
-  #AL D W MCMILLAN MEMORIAL HOSPITAL AL
-  #AR ARKANSAS METHODIST MEDICAL CENTER AR
-  #AZ JOHN C LINCOLN DEER VALLEY HOSPITAL AZ
-  #CA SHERMAN OAKS HOSPITAL CA
-  #CO SKY RIDGE MEDICAL CENTER CO
-  #CT MIDSTATE MEDICAL CENTER CT
-  #DC <NA> DC
-  #DE <NA> DE
-  #FL SOUTH FLORIDA BAPTIST HOSPITAL FL
-
-  tail(rankall("pneumonia", "worst"), 3)
-  #hospital state
-  #WI MAYO CLINIC HEALTH SYSTEM - NORTHLAND, INC WI
-  #WV PLATEAU MEDICAL CENTER WV
-  #WY NORTH BIG HORN HOSPITAL DISTRICT WY
-
-  tail(rankall("heart failure"), 10)
-  #hospital state
-  #TN WELLMONT HAWKINS COUNTY MEMORIAL HOSPITAL TN
-  #TX FORT DUNCAN MEDICAL CENTER TX
-  #UT VA SALT LAKE CITY HEALTHCARE - GEORGE E. WAHLEN VA MEDICAL CENTER UT
-  #VA SENTARA POTOMAC HOSPITAL VA
-  #VI GOV JUAN F LUIS HOSPITAL & MEDICAL CTR VI
-  #VT SPRINGFIELD HOSPITAL VT
-  #WA HARBORVIEW MEDICAL CENTER WA
-  #WI AURORA ST LUKES MEDICAL CENTER WI
-  #WV FAIRMONT GENERAL HOSPITAL WV
-  #WY CHEYENNE VA MEDICAL CENTER WY
-
+  print("test 1")
+  print(head( rankall("heart attack", 20), 10))
+  
+  print("test 2")
+  print(tail(rankall("pneumonia", "worst"), 3))
+  
+  print("test 3")
+  print(tail(rankall("heart failure"), 10))
+  
 }
